3c166abec0472e5685f11b6d617f676fb6e0d771
arige <417923140@qq.com>
2019-12-06 16:24:50 +0800

    【新增】okhttp和okio

---
+++ net/mtgokhttp/src/main/java/com/mintegral/msdk/thrid/okhttp/package-info.java
@@ -0,0 +1,5 @@
+/** An HTTP+HTTP/2 client for Android and Java applications. */
+@EverythingIsNonNull
+package com.mintegral.msdk.thrid.okhttp;
+
+import com.mintegral.msdk.thrid.okhttp.internal.annotations.EverythingIsNonNull;
