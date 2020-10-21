608c0e8b01298fd26dfa948a3017e59a93de3fe9
songjunjun <junjun.song@mobvista.com>
2020-10-21 15:16:08 +0800

    【修改】修改包名

---
+++ net/app/src/main/java/com/mbridge/msdk/thrid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mbridge.msdk.thrid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ net/mtgokhttp/src/main/java/com/mbridge/msdk/thrid/okhttp/package-info.java
@@ -0,0 +1,5 @@
+/** An HTTP+HTTP/2 client for Android and Java applications. */
+@EverythingIsNonNull
+package com.mbridge.msdk.thrid.okhttp;
+
+import com.mbridge.msdk.thrid.okhttp.internal.annotations.EverythingIsNonNull;
